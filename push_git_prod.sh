992d131e141e3637aeaf724fa6afe1af78447eda
n2hq <n2hq.co@gmail.com>
2025-11-21 14:58:12 +0100

    Production: update commit

---
+++ push_git_prod.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+APP_NAME="bycet_index"
+
+echo "🌿  Renaming branch to main..."
+git branch -M main
+
+
+echo "🔁 Setting remote to TEST repo..."
+git remote add origin https://github.com/n2hq/$APP_NAME.git || git remote set-url origin https://github.com/n2hq/$APP_NAME.git
+
+
+
+echo "🧪 Switching to 'main' branch..."
+git checkout main || git checkout -b main
+
+
+echo "🔄 Pulling latest changes..."
+git pull origin main --allow-unrelated-histories --no-rebase || echo "No existing history to pull"
+
+
+echo "Deleting build folder..."
+rm -rf build/ 2>/dev/null || true
+
+echo "🛠  Building PROD..."
+npm run build:prod
+#npm run build:prod
+
+echo "📦  Staging changes..."
+git add .
+
+
+echo "✅  Committing changes..."
+git commit -m "Production: update commit" || echo "⚠️ No changes to commit."
+
+echo "🌿  Renaming branch to main..."
+git branch -M main
+
+echo "🚀  Pushing to origin/main..."
+git push -u origin main
+
+echo "🎉  Done!"
+
+
